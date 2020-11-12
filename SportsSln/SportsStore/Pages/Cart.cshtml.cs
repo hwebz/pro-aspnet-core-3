981e2192ffffe5e7e66506735358703eaba67232
Ha Manh Do <hadm.haui@gmail.com>
2020-11-13 00:32:37 +0700

    Continue to do cart added

--- SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -13,9 +13,10 @@ namespace SportsStore.Pages
     {
         private IStoreRepository repository;
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public Cart Cart { get; set; }
@@ -23,15 +24,17 @@ namespace SportsStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
         {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == productId).Product);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
